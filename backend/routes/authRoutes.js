73a54d2df5bfe42daebf3c728218a3f840137043
IORD1 <pratham111ingole@gmail.com>
2023-03-24 22:00:08 +0530

    adding recruter's registration

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,4 +7,16 @@ router.get('/',(req, res)=>{
     res.send("Hello from Thorve Prasad");
 })
 
+// router.get('/auth',(req, res)=>{
+//     res.send("Hello from Pratham Ingole");
+// })
+
+
+router.get('/auth', async(req, res) => {
+    const findResult = await .find();
+    res.json(findResult);
+    await cursor.forEach(console.dir);
+    res.send("Hello from pratham");
+
+})
 module.exports = router;
